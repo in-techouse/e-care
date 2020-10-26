3f0cdd1cb3bbf5760715c7aabebce2269cf18614
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2020-10-26 20:24:17 +0500

    Working on EditProfile, NewAnnouncement, and NewProduct

--- app/src/main/java/kc/fyp/ecare/activities/EditProfile.java
+++ app/src/main/java/kc/fyp/ecare/activities/EditProfile.java
@@ -1,7 +1,9 @@
 package kc.fyp.ecare.activities;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -11,11 +13,20 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
+import br.com.simplepass.loadingbutton.customViews.CircularProgressButton;
 import kc.fyp.ecare.R;
+import kc.fyp.ecare.director.Session;
+import kc.fyp.ecare.models.User;
 
-public class EditProfile extends AppCompatActivity {
+public class EditProfile extends AppCompatActivity implements View.OnClickListener {
+    private CircularProgressButton action_update;
+    private User user;
+    private Session session;
+    private ImageView image;
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,14 +36,34 @@ public class EditProfile extends AppCompatActivity {
         CollapsingToolbarLayout toolBarLayout = findViewById(R.id.toolbar_layout);
         toolBarLayout.setTitle(getTitle());
 
+        session = new Session(EditProfile.this);
+        user = session.getUser();
+
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        action_update = findViewById(R.id.action_update);
+        image = findViewById(R.id.image);
+        fab.setOnClickListener(this);
+        action_update.setOnClickListener(this);
+
+        if (user.getImage() != null && user.getImage().length() > 0) {
+            Glide.with(getApplicationContext()).load(user.getImage()).into(image);
+        } else {
+            image.setImageDrawable(getResources().getDrawable(R.drawable.user));
+        }
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.fab: {
+                break;
             }
-        });
+            case R.id.action_update: {
+                break;
+            }
+        }
     }
 
     @Override
--- app/src/main/java/kc/fyp/ecare/activities/NewProduct.java
+++ app/src/main/java/kc/fyp/ecare/activities/NewProduct.java
@@ -1,10 +1,12 @@
 package kc.fyp.ecare.activities;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.smarteist.autoimageslider.SliderView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -12,9 +14,16 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
+import br.com.simplepass.loadingbutton.customViews.CircularProgressButton;
 import kc.fyp.ecare.R;
+import kc.fyp.ecare.director.Session;
+import kc.fyp.ecare.models.User;
 
-public class NewProduct extends AppCompatActivity {
+public class NewProduct extends AppCompatActivity implements View.OnClickListener {
+    private CircularProgressButton action_save;
+    private User user;
+    private Session session;
+    private SliderView imageSlider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +34,30 @@ public class NewProduct extends AppCompatActivity {
         CollapsingToolbarLayout toolBarLayout = findViewById(R.id.toolbar_layout);
         toolBarLayout.setTitle(getTitle());
 
+        session = new Session(NewProduct.this);
+        user = session.getUser();
+
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        action_save = findViewById(R.id.action_save);
+        imageSlider = findViewById(R.id.imageSlider);
+        fab.setOnClickListener(this);
+        action_save.setOnClickListener(this);
     }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.fab: {
+                break;
+            }
+            case R.id.action_save: {
+                break;
+            }
+        }
+    }
+
 
     @Override
     public void onBackPressed() {
